31097cd5b3318132022f77fdaa924e3029d6b53b
sunny200745 <sunny.200745@gmail.com>
2015-01-09 20:40:58 +0530

    Linking to submit button on login page

--- app/lib/controllers.js
+++ app/lib/controllers.js
@@ -8,9 +8,13 @@ angular.module('xeApp.controllers', ['ui.router'])
 	}])
 	.controller('xeFooter',['$scope',function($scope){
 	}])
-	.controller('xeMain',['$scope',function($scope){
+	.controller('xeMain',['$scope','$location',function($scope,$location){
+		$scope.fn_login = function(){			
+			$location.path( "/dashboard" );
+		};
 	}])
 	.controller('xeDashboard',['$scope',function($scope){
+
 	}])
 	.controller('xeUserProfile',['$scope', '$stateParams', function($scope, $stateParams){
 		$stateParams.contactId
